4b175e5220db50533dba8345569d539634fdf25d
summylight <summylight@163.com>
2020-05-19 11:08:47 +0800

    main

--- rwh.h
+++ rwh.h
@@ -1,2 +1,11 @@
 /* main:
-    cmdline-> read graphl -> random walk -> (Convergence judgment) -> 
+    cmdline-> read graphl -> random walk -> (Convergence judgment) -> hashcode -> next randomwalk -> ... -> hashencode -> graphlet ios -> compute count -> output
+    */
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]){
+    if (argc < 1) {
+        printf("")
+    }
+}
