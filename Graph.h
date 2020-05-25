ff7c6e411bbfb2678cf3dab7b0d81e65939f12c3
Light Summy <summylight@163.com>
2020-05-25 10:29:41 +0800

    1

--- Graph.h
+++ Graph.h
@@ -9,15 +9,18 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include "Graphlet.h"
 #include "alg.h"
 using namespace std;
 
+
 class Graph{
     public:
-        Graph(char *filename);// read graph into vector
+        void read_graph(char *filename);// read graph into vector
         int get_types();
         int get_nodes();
         vector<int> get_neighs(int v);
+        void random_sample();
 
     private:
         vector<vector<int>> G;
